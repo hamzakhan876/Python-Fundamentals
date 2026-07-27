743763e8e72c559d70761232462852dc453e0c04
Hamza Ahmed khan <hamzaahmedkhan3910@gmail.com>
2026-07-27 16:36:24 -0700

    Add files via upload

---
+++ Project 02 Text_file analyzer.py
@@ -0,0 +1,37 @@
+file  = open("Data.Txt", "w")
+file.write("Iam learning Python!")
+file.close()
+
+file = open("Data.Txt", "r")
+data = file.read()
+file.close()
+
+
+file = open("Data.Txt", "a")
+file.write("\nIam Enjoying it!")
+file.write("\nPython is easy")
+file.write("\nI like Python")
+file.close()
+
+
+file = open("Data.Txt","r")
+
+text = file.read()
+
+file.close()
+
+lines = text.split("\n")
+
+print(len(lines))
+
+words = text.split()
+
+unique_words = set(words)
+
+def menu():
+    print("========= TEXT FILE ANALYZER =========")
+    print("Total Lines :", len(lines))
+    print("Total Words :", len(words))
+    print("Unique Words:", len(unique_words))
+
+menu()
